302ab56120439914aaf0f1945b3f9beaa13956a6
Steve Cosman <scosman@gmail.com>
2012-07-19 15:08:48 -0400

    blank rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Betakit::Application.config.secret_token = 'ff0debde83bdfdd57d1c82bca2d59d819d6e58844775b25f0ad6b08a9baa75dd79464cf3f26688d53745c3a87671cdab8dbfaaf727ee12b5e77fccf4ab7529fe'
